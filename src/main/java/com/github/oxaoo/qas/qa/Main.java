88bcc8170ff0affea99cd6d22287f52fa62b0198
akulesho <Aleksandr.Kuleshov@t-systems.ru>
2016-10-29 20:55:26 +0300

    added the POS tagging for syntactic analyze

--- src/main/java/com/github/oxaoo/qas/qa/Main.java
+++ src/main/java/com/github/oxaoo/qas/qa/Main.java
@@ -1,21 +1,26 @@
 package com.github.oxaoo.qas.qa;
 
 import com.github.oxaoo.qas.syntax.parse.SyntaxAnalyzer;
+import com.github.oxaoo.qas.syntax.tagging.PosTagging;
 import com.github.oxaoo.qas.syntax.utils.SyntaxUtils;
+import org.annolab.tt4j.TreeTaggerException;
 import org.maltparser.core.exception.MaltChainedException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
 import java.util.List;
 
 public class Main {
     private static final Logger LOG = LoggerFactory.getLogger(Main.class);
 
-    public static void main(String[] args) throws MaltChainedException {
-        final SyntaxAnalyzer syntax = new SyntaxAnalyzer();
-        final boolean resultSyntax = syntax.analyze();
-        LOG.info("Result of syntax analyze: {}", resultSyntax);
+    public static void main(String[] args) throws MaltChainedException, IOException, TreeTaggerException {
+//        final SyntaxAnalyzer syntax = new SyntaxAnalyzer();
+//        final boolean resultSyntax = syntax.analyze();
+//        LOG.info("Result of syntax analyze: {}", resultSyntax);
 //        simpleTextConverter();
+        PosTagging pos = new PosTagging();
+        pos.tagging();
     }
 
     public static void simpleTextConverter() {
---
+++ src/main/java/com/github/oxaoo/qas/syntax/tagging/PosTagging.java
@@ -0,0 +1,37 @@
+package com.github.oxaoo.qas.syntax.tagging;
+
+import org.annolab.tt4j.TokenHandler;
+import org.annolab.tt4j.TreeTaggerException;
+import org.annolab.tt4j.TreeTaggerWrapper;
+
+import java.io.IOException;
+
+/**
+ * The class represent the Part-of-Speech tagging
+ */
+public class PosTagging {
+
+    // Point TT4J to the TreeTagger installation directory. The executable is expected
+    // in the "bin" subdirectory - in this example at "/opt/treetagger/bin/tree-tagger"
+    public void tagging() throws IOException, TreeTaggerException {
+        System.setProperty("treetagger.home", "E:/Study/dev/TreeTagger");
+        TreeTaggerWrapper<String> tt = new TreeTaggerWrapper<String>();
+        try {
+//            tt.setModel("E:/Study/dev/TreeTagger/lib/english-utf8.par:iso8859-1");
+            tt.setModel("E:/Study/dev/TreeTagger/lib/russian-utf8.par");
+            tt.setHandler(new TokenHandler<String>()
+            {
+                public void token(String token, String pos, String lemma)
+                {
+                    System.out.println(token + "\t" + pos + "\t" + lemma);
+                }
+            });
+//            tt.process(new String[] { "This", "is", "a", "test", "." });
+//            tt.process(new String[] { "Это", "простой", "текстик", "." });
+            tt.process(new String[] { "Данное", "программное", "обеспечение", "работает", "корректно", "." });
+        }
+        finally {
+            tt.destroy();
+        }
+    }
+}
