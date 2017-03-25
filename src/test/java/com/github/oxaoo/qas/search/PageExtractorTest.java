c1f17a211ddc07339ae3bc4e409436e68a4817c3
oxaoo <yol0317@gmail.com>
2017-03-25 16:28:43 +0300

    added simple test 'extractRelevantSentencesTest'

--- src/test/java/com/github/oxaoo/qas/search/PageExtractorTest.java
+++ src/test/java/com/github/oxaoo/qas/search/PageExtractorTest.java
@@ -37,17 +37,37 @@ public class PageExtractorTest {
     @Test
     public void encodedQueryTest() {
         String link = "https://ru.wikipedia.org/wiki/%D0%AD%D0%BB%D1%8C%D0%B1%D1%80%D1%83%D1%81";
-        pageExtractor.extract(Collections.singletonList(new Result().setLink(link)));
+        PageExtractor.extract(Collections.singletonList(new Result().setLink(link)));
     }
 
     @Test
     public void simpleQueryTest() {
         String link = "https://ru.wikipedia.org/wiki/Эльбрус";
-        pageExtractor.extract(Collections.singletonList(new Result().setLink(link)));
+        PageExtractor.extract(Collections.singletonList(new Result().setLink(link)));
     }
 
     @Test
     public void extractRelevantSentencesTest() {
+        String link = "https://ru.wikipedia.org/wiki/%D0%AD%D0%BB%D1%8C%D0%B1%D1%80%D1%83%D1%81";
+        String stubSnippet = "Эльбру́с (карач.-балк. Минги Тау, кабард.-черк. Ошхамахо ) — стратовулкан \n" +
+                "на Кавказе ... Эльбрус находится в Боковом хребте Большого Кавказа в 10 \n" +
+                "километрах к северу от Главного Кавказского хребта на границе республик ... ";
+        Result result = new Result().setLink(link).setSnippet(stubSnippet);
+        List<String> relevantFragments = RelevantInfoExtractor.extract(Collections.singletonList(result));
+        LOG.info("Relevant fragments:");
+        relevantFragments.forEach(s -> LOG.info("{}\n\n", s));
+    }
+
+
+
+
+
+
+
+
+
+    @Test
+    public void extractRelevantSentencesTest_() {
         String link = "https://ru.wikipedia.org/wiki/%D0%AD%D0%BB%D1%8C%D0%B1%D1%80%D1%83%D1%81";
         String stubSnippet = "Эльбру́с (карач.-балк. Минги Тау, кабард.-черк. Ошхамахо ) — стратовулкан \n" +
                 "на Кавказе ... Эльбрус находится в Боковом хребте Большого Кавказа в 10 \n" +
